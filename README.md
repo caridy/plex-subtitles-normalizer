ac4810b93d3b02d92088c8ec4af86fc9af849e51
Caridy Patino <caridy@gmail.com>
2014-03-02 20:24:04 -0500

    using nopt for arguments, adding encoding options, from-backup, etc

--- README.md
+++ README.md
@@ -27,11 +27,38 @@ $ cd path/to/Movies
 $ psn
 ```
 
-By calling `psn` in a folder, it will automatically recurvisely for any file with `SRT` extension, it will analyze the file looking for invalid formats, if the file has any, it will create a backup of the file (by adding the `.bak` extension), and it will override the original file with the correct version of the SRT format.
+By calling `psn` in a folder, it looks for any file with `.srt` extension, analyzing the file looking for invalid formats, if the file has any, it will create a backup of the file (by adding the `.bak` extension), and it will override the original file with the correct format.
 
-Additionally, you can pass an option to avoid creating backup files:
+### Advanced Options
+
+#### Backups
+
+You can pass an option to avoid creating backup files, although it is not recommended:
 
 ```
-$ cd path/to/TV-shows
-$ psn --nobackup
+$ psn --no-backup
+```
+
+If something weird happen, you can always restore the backup, or simply use the backup as the original source:
+
+```
+$ psn --from-backup
+```
+
+#### Encoding
+
+You can also select the encoding, by default `latin1` will be used. The tool will automatically detect some encoding issues, but if you find something weird, you can try to use an alternative encoding from [iconv-lite][], for example, you can use `Big5` for chinese:
+
+```
+$ psn --from-backup --encoding Big5
+```
+
+[iconv-lite]: https://github.com/ashtuchkin/iconv-lite#supported-encodings
+
+#### Custom path
+
+You can customize the pattern to search for subtitles:
+
+```
+$ psn --pattern **/*.srt
 ```
--- cli.js
+++ cli.js
@@ -2,10 +2,11 @@
 
 var libfs = require('fs'),
     glob = require("glob"),
+    nopt = require("nopt"),
     libpath = require('path'),
     log = require('./lib/log'),
-    parser = require('subtitles-parser');
-
+    parser = require('subtitles-parser'),
+    iconvlite = require('iconv-lite');
 
 function generator(data) {
     var i,
@@ -22,65 +23,86 @@ function generator(data) {
 }
 
 function main(argv, cwd, cb) {
-    var output, original, data, files, i; // {command:"…", args:{…}, opts:{…}}
+    var output, original, data, files, i,
+        options = nopt({
+            "pattern": [String, null],
+            "encoding": [String, null],
+            "backup": [Boolean, null],
+            "from-backup": [Boolean, null]
+        });
 
-    log.level = 'info';
+    options.encoding = options.encoding || 'latin1';
+    options.backup = options.backup !== false ? true : false;
+    options.pattern = options.pattern || "**/*.srt";
+
+    log.info('Encoding in [' + options.encoding + ']');
+    log.info('Folder [' + cwd + ']');
+    log.info('Looking for [' + options.pattern + ']');
 
     // options is optional
-    files = glob.sync("**/*.srt");
+    files = glob.sync(options.pattern);
     if (files.length === 0) {
-        log.info('No SRT files founded within folder: ' + process.cwd);
+        log.info('No file found: ' + cwd);
         return cb();
     }
     for (i = 0; i < files.length; i += 1) {
-        log.info('Processing file: ' + files[i]);
+        log.info('\nFile [' + files[i] + ']');
 
         try {
-            original = libfs.readFileSync(libpath.join(cwd, files[i]), 'utf8');
+            original = iconvlite.decode(libfs.readFileSync(libpath.join(cwd, files[i] + (options["from-backup"] ? '.bak' : ''))), options.encoding);
         } catch (e) {
-            log.error('* Reading error: ' + (e.stack || e));
+            log.error(' -> Reading error: ' + (e.stack || e));
             continue;
         }
 
         try {
             data = parser.fromSrt(original);
         } catch (e) {
-            log.error('* Parser error: ' + (e.stack || e));
+            log.error(' -> Parser error: ' + (e.stack || e));
             continue;
         }
 
         try {
             output = generator(data);
         } catch (e) {
-            log.error('* Generator error: ' + (e.stack || e));
+            log.error(' -> Generator error: ' + (e.stack || e));
             continue;
         }
 
         if (output === original) {
-            log.info('* Valid SRT format, skipping.');
+            log.info(' -> Valid SRT format, skipping.');
             continue;
         }
 
-        if (argv.indexOf('--nobackup') === -1) {
-            log.info('* Backing up file (*.bak)');
-            try {
-                libfs.writeFileSync(libpath.join(cwd, files[i] + '.bak'), output);
-            } catch (e) {
-                log.error('* Backup error: ' + (e.stack || e));
-                continue;
+        if (output.indexOf('�') > -1) {
+            log.error(' -> Invalid encoding, set a valid encoding value `--enconding <value>`.');
+            continue;
+        }
+
+        if (options.backup && !options['from-backup']) {
+            if (!libfs.existsSync(libpath.join(cwd, files[i] + '.bak'))) {
+                log.info(' -> Backing up file (*.bak)');
+                try {
+                    libfs.writeFileSync(libpath.join(cwd, files[i] + '.bak'), output);
+                } catch (e) {
+                    log.error(' -> Backup error: ' + (e.stack || e));
+                    continue;
+                }
+            } else {
+                log.info(' -> Backup already exists');
             }
         }
 
-        log.info('* Overwritting file');
+        log.info(' -> Overwritting file');
         try {
             libfs.writeFileSync(libpath.join(cwd, files[i]), output);
         } catch (e) {
-            log.error('* Write error: ' + (e.stack || e));
+            log.error(' -> Write error: ' + (e.stack || e));
             continue;
         }
-        log.info('* Done with ' + files[i]);
+        log.info(' -> Done');
     }
-    log.info('OK');
+    log.info('\nOK');
     return cb(null);
 }
 
